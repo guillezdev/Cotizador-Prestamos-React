fbbdb6cc11cd5f39f458b4479c1e3cf7afb509ae
guillezdev <guillermoprogcc@gmail.com>
2023-05-27 22:07:35 -0700

    created components folder and the frist part of my app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import { Header } from "./components/Header"
+
+
 function App() {
- 
+
   return (
     <>
-        <h1 className="text-slate-900 font-bold text-2xl">Hello world</h1>
+      <div className="mx-auto my-5 px-4 bg-white shadow rounded-sm min-h-[200px] max-w-lg text-center">
+         <Header />
+
+         <input type="range" className="w-full h-2 bg-gray-50 text-teal-500 hover:text-teal-600" />
+      </div>
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export function Header() {
+    return (
+        <>
+           <h1 className="text-slate-800 font-bold text-2xl">Cuanto <span className="text-indigo-500">dinero</span> necesitas?</h1>
+        </>
+    )
+    
+}
