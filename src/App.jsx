3abaf6e2eadf2b440bfebce51bc787e5341f70c2
guillezdev <guillermoprogcc@gmail.com>
2023-05-28 20:37:19 -0700

    first version V1.0 Cotizador de prestamos

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
-import { Header } from "./components/Header"
-import { useState } from 'react'
+import { Header } from "./components/Header";
+import { useEffect, useState } from "react";
 import ButtonControls from "./components/ButtonControls";
-import {formatearDinero} from './helpers'
+import {
+  formatearDinero,
+  calcularTotalPagar,
+  calcularPagoMensual,
+} from "./helpers";
 function App() {
   const [range, setRange] = useState(0);
+  const [meses, setMeses] = useState(6);
+  const [total, setTotal] = useState(0);
+  const [pagoMensual, setPagoMensual] = useState(0);
 
-  const handleRange = (el) => { setRange(Number(el.target.value)) };
+  useEffect(() => {
+    setTotal(calcularTotalPagar(range, meses));
+  }, [range, meses]);
+
+  useEffect(() => {
+    setPagoMensual(calcularPagoMensual(total, meses));
+  }, [total]);
+
+  const handleRange = (el) => {
+    setRange(Number(el.target.value));
+  };
 
   return (
     <>
-      <div className="mx-auto my-5 px-10 bg-white shadow rounded-sm min-h-[200px] max-w-lg text-center">
+      <main className="mx-auto my-5 p-10 bg-white shadow rounded-sm  max-w-md text-center">
         <Header />
         <input
           type="range"
@@ -20,11 +37,40 @@ function App() {
           max={20000}
           step={100}
         />
-        <ButtonControls setRange={setRange}/>
-        <span className="text-xl text-indigo-500 space-x-0">{formatearDinero(range)}</span>
-      </div>
+        <ButtonControls setRange={setRange} />
+        <span className="text-xl text-indigo-500 space-x-0">
+          {formatearDinero(range)}
+        </span>
+        <h2 className="text-lg font-semibold">
+          Elige un <span className="text-blue-500">plazo</span> a pagar
+        </h2>
+        <select
+          name=""
+          id=""
+          value={meses}
+          onChange={(op) => {
+            setMeses(Number(op.target.value));
+          }}
+          className="my-5 w-full  bg-white text-center border rounded-lg text-gray-600  border-gray-400"
+        >
+          <option value="6">6</option>
+          <option value="12">12</option>
+          <option value="24">24</option>
+        </select>
+        <div className="bg-blue-100 m-4 p-2">
+          <h2 className="text-lg font-semibold">
+            Resumen <span className="text-blue-500">de pago</span>
+          </h2>
+          <div className="w-full h-[1px] bg-slate-900"></div>
+          <ul className="font-semibold">
+            <li>{meses}</li>
+            <li>Total a pagar:{formatearDinero(total)}</li>
+            <li>Mensual:{formatearDinero(pagoMensual)}</li>
+          </ul>
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,7 +1,41 @@
-export const formatearDinero = (valor) => { 
-    const formatter = new Intl.NumberFormat('en-US',{
-        style:'currency',
-        currency:'USD'
-    });
-    return formatter.format(valor)
- }
+export const formatearDinero = (valor) => {
+  const formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
+  return formatter.format(valor);
+};
+
+export const calcularTotalPagar = (cantidad, plazo) => {
+  let total;
+
+  if (cantidad < 5000) {
+    total = cantidad * 1.5;
+  } else if (cantidad >= 5000 && cantidad < 10000) {
+    total = cantidad * 1.4;
+  } else if (cantidad >= 10000 && cantidad < 15000) {
+    total = cantidad * 1.3;
+  } else {
+    total = cantidad * 1.2;
+  }
+
+  //Plazo
+
+  if (plazo === 6) {
+    total = total * 1.1;
+  } else if (plazo === 12) {
+    total = total * 1.2;
+  } else {
+    total = total * 1.3;
+  }
+
+  return total;
+};
+
+export const calcularPagoMensual = (total, meses) => {
+  let pagototal;
+
+  pagototal = total / meses;
+
+  return pagototal;
+};
